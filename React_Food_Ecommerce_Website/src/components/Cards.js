7742d9736f6f21cc92cb66cdff2a9a8fb1e1c8c2
Riya Verma <vermariya522@gmail.com>
2023-01-01 23:04:20 +0530

    ui changes

--- React_Food_Ecommerce_Website/src/components/Cards.js
+++ React_Food_Ecommerce_Website/src/components/Cards.js
@@ -11,108 +11,99 @@ import CardFood from "./CardFood";
 import Set from "./Set";
 
 const Cards = () => {
-	const [data, setData] = useState(Cardsdata);
-	const getdata = useSelector((state) => state.cartreducer.carts);
+  const [data, setData] = useState(Cardsdata);
+  const getdata = useSelector((state) => state.cartreducer.carts);
 
-	const dispatch = useDispatch();
-	// const setText = "Items added to cart";
-	// const [showModal, setShowModal] = useState(true);
-	// const handleClose = () => {
-	// 	setShowModal(false);
-	// };
+  const dispatch = useDispatch();
+  // const setText = "Items added to cart";
+  // const [showModal, setShowModal] = useState(true);
+  // const handleClose = () => {
+  // 	setShowModal(false);
+  // };
 
-	const dlt = (id) => {
-		dispatch(DLT(id));
-	};
+  const dlt = (id) => {
+    dispatch(DLT(id));
+  };
 
-	const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-	const [copydata, setCopyData] = useState([]);
+  const [copydata, setCopyData] = useState([]);
 
-	const chanegData = (e) => {
-		let getchangedata = e.toLowerCase();
-		if (getchangedata == "") {
-			setCopyData(data);
-		} else {
-			let storedata = copydata.filter((ele, k) => {
-				return ele.rname.toLowerCase().match(getchangedata);
-			});
+  const chanegData = (e) => {
+    let getchangedata = e.toLowerCase();
+    if (getchangedata == "") {
+      setCopyData(data);
+    } else {
+      let storedata = copydata.filter((ele, k) => {
+        return ele.rname.toLowerCase().match(getchangedata);
+      });
 
-			setCopyData(storedata);
-		}
-	};
+      setCopyData(storedata);
+    }
+  };
 
-	const send = (e) => {
-		dispatch(ADD(e));
-	};
+  const send = (e) => {
+    dispatch(ADD(e));
+  };
 
-	useEffect(() => {
-		setTimeout(() => {
-			setCopyData(Cardsdata);
-		}, 3000);
-	}, []);
-	return (
-		<div className="container-fluid mx-0 px-0 py-0">
-			<Carousel>
-				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src="https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp"
-						alt="First slide"
-						style={{ height: "700px" }}
-					/>
-					<Carousel.Caption>
-						<h3>First slide label</h3>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src="https://b.zmtcdn.com/data/pictures/chains/5/19295245/089cbcf1d3307542c72f77272556b28b_o2_featured_v2.jpg?output-format=webp"
-						alt="Second slide"
-						style={{ height: "700px" }}
-					/>
+  useEffect(() => {
+    setTimeout(() => {
+      setCopyData(Cardsdata);
+    }, 3000);
+  }, []);
+  return (
+    <div className="container-fluid mx-0 px-0 py-0">
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp"
+            alt="First slide"
+            style={{ height: "700px" }}
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://b.zmtcdn.com/data/pictures/chains/5/19295245/089cbcf1d3307542c72f77272556b28b_o2_featured_v2.jpg?output-format=webp"
+            alt="Second slide"
+            style={{ height: "700px" }}
+          />
 
-					<Carousel.Caption>
-						<h3>Second slide label</h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img
-						className="d-block w-100"
-						src="https://b.zmtcdn.com/data/pictures/chains/5/110225/3978e28854b7496dbef9496546734811_o2_featured_v2.jpg"
-						alt="Third slide"
-						style={{ height: "700px" }}
-					/>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src="https://b.zmtcdn.com/data/pictures/chains/5/110225/3978e28854b7496dbef9496546734811_o2_featured_v2.jpg" alt="Third slide" style={{ height: "700px" }} />
 
-					<Carousel.Caption>
-						<h3>Third slide label</h3>
-						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-			</Carousel>
-			<div className="row mt-2">
-				<Form className="d-flex justify-content-center align-items-center mt-3">
-					<Form.Group className=" mx-2 col-lg-4" controlId="formBasicEmail">
-						<Form.Control type="text" onChange={(e) => chanegData(e.target.value)} placeholder="Search Restaurant" />
-					</Form.Group>
-				</Form>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div className="row mt-2">
+        <Form className="d-flex justify-content-center align-items-center mt-3">
+          <Form.Group className=" mx-2 col-lg-4" controlId="formBasicEmail">
+            <Form.Control type="text" onChange={(e) => chanegData(e.target.value)} placeholder="Search Restaurant" />
+          </Form.Group>
+        </Form>
 
-				<div className="row d-flex justify-content-center align-items-center">
-					{data.map((element, id) => {
-						return (
-							<>
-								{copydata && copydata.length ? <CardFood data={copydata} /> : <Set sdata={data} />}
-								{modalOpen && <Model setOpenModal={setModalOpen} />}
-							</>
-						);
-					})}
-				</div>
-			</div>
-		</div>
-	);
+        <div className="row d-flex justify-content-center align-items-center mb-4">
+          <>
+            {copydata && copydata.length ? <CardFood data={copydata} /> : <Set sdata={data} />}
+            {modalOpen && <Model setOpenModal={setModalOpen} />}
+          </>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- React_Food_Ecommerce_Website/src/components/Header.js
+++ React_Food_Ecommerce_Website/src/components/Header.js
@@ -12,168 +12,205 @@ import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import { useNavigate } from "react-router-dom";
-import logo from "./assets/logo.png"
-import Form from "react-bootstrap/Form";
+import logo from "./assets/logo.png";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Header = () => {
-	const getdata = useSelector((state) => state.cartreducer.carts);
-	const [anchorEl, setAnchorEl] = useState(null);
-	const [price, setPrice] = useState(0);
+  const getdata = useSelector((state) => state.cartreducer.carts);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [price, setPrice] = useState(0);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
-	const open = Boolean(anchorEl);
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const dispatch = useDispatch();
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const dispatch = useDispatch();
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	const history = useNavigate();
+  const openNav = () => {
+    setNavbarOpen((prev) => !prev);
+  };
 
-	const dlt = (id) => {
-		dispatch(DLT(id));
-	};
+  const history = useNavigate();
 
-	const total = () => {
-		let price = 0;
-		getdata.map((ele) => {
-			price = ele.price * ele.qnty + price;
-			return price;
-		});
-		setPrice(price);
-	};
+  const dlt = (id) => {
+    dispatch(DLT(id));
+  };
 
-	const ready = () => {
-		history("/login");
-	};
+  const total = () => {
+    let price = 0;
+    getdata.map((ele) => {
+      price = ele.price * ele.qnty + price;
+      return price;
+    });
+    setPrice(price);
+  };
 
-	useEffect(() => {
-		total();
-	}, [total]);
+  const ready = () => {
+    history("/login");
+  };
 
-	return (
-		<>
-			<Navbar bg="bg-secondary" variant="dark" style={{ height: "60px" ,backgroundColor : "#C8AD7F"}}>
-				<Container>
-				<NavLink to="/" >
-                  <img src={logo} style={{ width: "8rem", height: "8rem",marginLeft :"0rem" }} alt="" />
-				</NavLink>
-					<Nav className="me-auto">
-						<NavLink to="/" className="text-decoration-none text-dark mx-3" style={{ fontSize: 15,fontWeight : "bold", }}>
-							Home
-						</NavLink>
-						<NavLink to="/contactus" className="text-decoration-none text-dark
-						" style={{ fontSize: 15,fontWeight : "bold" }}>
-							Contact Us
-						</NavLink>
-					</Nav>
+  useEffect(() => {
+    total();
+  }, [total]);
 
-					<div className="search-wrapper">
-                        <label htmlFor="search-form">
-                            <input
-                                type="search"
-                                name="search-form"
-                                id="search-form"
-                                className="search-input"
-                                placeholder="Search for..."
-                              //  value={q}
-                                /*
-                                // set the value of our useState q
-                                //  anytime the user types in the search box
-                                */
-                              //  onChange={(e) => setQ(e.target.value)}
-                            />
-                            <span className="sr-only">Search countries here</span>
-                        </label>
-                    </div>
-					<Badge
-						badgeContent={getdata.length}
-						color="primary"
-						id="fade-button"
-						aria-controls={open ? "fade-menu" : undefined}
-						aria-haspopup="true"
-						aria-expanded={open ? "true" : undefined}
-						onClick={handleClick}
-					>
-						<i className="fa-solid fa-cart-shopping text-dark" style={{ fontSize: 25, cursor: "pointer" }}></i>
-					</Badge>
-					<div className="mx-3">
-						<PersonOutlineIcon style={{ color: "black", fontSize: 28 , cursor: "pointer"}} onClick={() => ready()}></PersonOutlineIcon>
-					</div>
-				</Container>
-				<Menu
-					id="fade-menu"
-					MenuListProps={{
-						"aria-labelledby": "fade-button",
-					}}
-					anchorEl={anchorEl}
-					open={open}
-					onClose={handleClose}
-					TransitionComponent={Fade}
-				>
-					{getdata.length ? (
-						<div className="card_details" style={{ width: "24rem", padding: 10 }}>
-							<Table>
-								<thead>
-									<tr>
-										<th>Photo</th>
-										<th>Restaurant Name</th>
-									</tr>
-								</thead>
-								<tbody>
-									{getdata.map((e) => {
-										return (
-											<>
-												<tr>
-													<td>
-														<NavLink to={`/cart/${e.id}`} onClick={handleClose}>
-															<img src={e.imgdata} style={{ width: "5rem", height: "5rem" }} alt="" />
-														</NavLink>
-													</td>
-													<td>
-														<p>{e.rname}</p>
-														<p>Price : ₹{e.price}</p>
-														<p>Quantity : {e.qnty}</p>
-														<p style={{ color: "red", fontSize: 20, cursor: "pointer" }} onClick={() => dlt(e.id)}>
-															<i className="fas fa-trash smalltrash"></i>
-														</p>
-													</td>
+  return (
+    <>
+      <div className="position-relative">
+        <nav className="navbar navbar-expand-lg border-bottom bg-white sticky-top shadow-sm navbar-light" id="header">
+          <div className="container-fluid">
+            <div className="d-flex">
+              <a
+                href="/"
+                className="navbar-brand "
+                style={{
+                  fontSize: "25px",
+                  fontWeight: "700",
+                  paddingLeft: "40px",
+                }}
+              >
+                Go<span className="head"> || Food</span>
+              </a>
+            </div>
+            <div className="top mt-4">
+              <ul open={open} className={`navbar-nav ${navbarOpen ? " showMenu" : ""}`} id="navbar">
+                <li className="nav-item active ">
+                  <a className="nav-link " href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/cart">
+                    Contacts
+                  </a>
+                </li>
 
-													<td className="mt-5" style={{ color: "red", fontSize: 20, cursor: "pointer" }} onClick={() => dlt(e.id)}>
-														<i className="fas fa-trash largetrash"></i>
-													</td>
-												</tr>
-											</>
-										);
-									})}
-								</tbody>
-							</Table>
+                <li className="nav-item">
+                  <a className="nav-link" href="/items">
+                    Shop
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/aboutus">
+                    AboutUs
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-							<div className="row mx-2">
-								<div className="col-sm-6">
-									<p className="text-center">Total :₹ {price}</p>
-								</div>
+            <div className="icon-header">
+              <PersonOutlineIcon className="border" style={{ fontSize: 28, cursor: "pointer" }} onClick={() => ready()}></PersonOutlineIcon>
+              <FavoriteBorderIcon
+                className="border"
+                style={{
+                  color: "red",
+                  fontSize: 28,
+                  cursor: "pointer",
+                  marginLeft: "1rem",
+                }}
+              ></FavoriteBorderIcon>
+              <Badge
+                badgeContent={getdata.length}
+                color="primary"
+                id="fade-button"
+                aria-controls={open ? "fade-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <ShoppingCartOutlinedIcon
+                  className="border"
+                  style={{
+                    fontSize: 28,
+                    cursor: "pointer",
+                    marginLeft: "1rem",
+                  }}
+                ></ShoppingCartOutlinedIcon>
+              </Badge>
+              {/* <div className="mobile">
+              <i className="fa-solid fa-bars" open={open} onClick={openNav}>{navbarOpen ? "" : ""}</i>
+            </div> */}
+            </div>
+          </div>
 
-								<div className="col-sm-6">
-									<button className="btn text-light col-lg-10" style={{ background: "#ed4c67" }}>
-										Checkout
-									</button>
-								</div>
-							</div>
-						</div>
-					) : (
-						<div className="card_details d-flex justify-content-center align-items-center" style={{ width: "24rem", padding: 10, position: "relative" }}>
-							<i className="fas fa-close smallclose" onClick={handleClose} style={{ position: "absolute", top: 2, right: 20, fontSize: 23, cursor: "pointer" }}></i>
-							<p style={{ fontSize: 22 }}>Your carts is empty</p>
-							<img src="./cart.gif" alt="" className="emptycart_img" style={{ width: "5rem", padding: 10 }} />
-						</div>
-					)}
-				</Menu>
-			</Navbar>
-		</>
-	);
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            {getdata.length ? (
+              <div className="card_details" style={{ width: "24rem", padding: 10 }}>
+                <Table>
+                  <thead>
+                    <tr>
+                      <th>Photo</th>
+                      <th>Restaurant Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {getdata.map((e) => {
+                      return (
+                        <>
+                          <tr>
+                            <td>
+                              <NavLink to={`/cart/${e.id}`} onClick={handleClose}>
+                                <img src={e.imgdata} style={{ width: "5rem", height: "5rem" }} alt="" />
+                              </NavLink>
+                            </td>
+                            <td>
+                              <p>{e.rname}</p>
+                              <p>Price : ₹{e.price}</p>
+                              <p>Quantity : {e.qnty}</p>
+                              <p style={{ color: "red", fontSize: 20, cursor: "pointer" }} onClick={() => dlt(e.id)}>
+                                <i className="fas fa-trash smalltrash"></i>
+                              </p>
+                            </td>
+
+                            <td className="mt-5" style={{ color: "red", fontSize: 20, cursor: "pointer" }} onClick={() => dlt(e.id)}>
+                              <i className="fas fa-trash largetrash"></i>
+                            </td>
+                          </tr>
+                        </>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+
+                <div className="row mx-2">
+                  <div className="col-sm-6">
+                    <p className="text-center">Total :₹ {price}</p>
+                  </div>
+
+                  <div className="col-sm-6">
+                    <button className="btn text-light col-lg-10" style={{ background: "#ed4c67" }}>
+                      Checkout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="card_details d-flex justify-content-center align-items-center" style={{ width: "24rem", padding: 10, position: "relative" }}>
+                <i className="fas fa-close smallclose" onClick={handleClose} style={{ position: "absolute", top: 2, right: 20, fontSize: 23, cursor: "pointer" }}></i>
+                <p style={{ fontSize: 22 }}>Your carts is empty</p>
+                <img src="./cart.gif" alt="" className="emptycart_img" style={{ width: "5rem", padding: 10 }} />
+              </div>
+            )}
+          </Menu>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Header;
